f1118edbae61c53a7b4c19b04955c8f283dda60e
Ameynuddin <md.ameynuddin@gmail.com>
2025-01-05 08:27:14 +0800

    updated Users

--- ccsd/src/main/java/com/example/ccsd/Users/usersService.java
+++ ccsd/src/main/java/com/example/ccsd/Users/usersService.java
@@ -4,6 +4,9 @@ package com.example.ccsd.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class usersService {
@@ -11,6 +14,32 @@ public class usersService {
     @Autowired
     private usersRepository usersRepository;
 
- 
-    
+    public List<users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public Optional<users> getUserById(String id) {
+        return usersRepository.findById(id);
+    }
+
+    public users addUser(users user) {
+        return usersRepository.save(user);
+    }
+
+    public users updateUser(String id, users userDetails) {
+        Optional<users> user = usersRepository.findById(id);
+        if (user.isPresent()) {
+            userDetails.setId(id);
+            return usersRepository.save(userDetails);
+        }
+        return null;
+    }
+
+    public void deleteUser(String id) {
+        usersRepository.deleteById(id);
+    }
+
+    public users getUserByEmail(String email) {
+        return usersRepository.findByEmail(email);
+    }
 }
